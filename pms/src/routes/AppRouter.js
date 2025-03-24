663033714b78afc4845d7a9fe8b6b48c65c2fc29
GiannisKav7 <58071478+GiannisKav7@users.noreply.github.com>
2025-03-24 15:57:57 +0200

    Update AppRouter

--- pms/src/routes/AppRouter.js
+++ pms/src/routes/AppRouter.js
@@ -15,7 +15,7 @@ import UnitTypePage from '../pages/UnitTypePage';
 
 const AppRouter = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
@@ -32,7 +32,7 @@ const AppRouter = () => {
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
